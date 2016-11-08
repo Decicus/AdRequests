5585481599844fce965f3014dc21aab227a31822
Alex Thomassen <alex@thomassen.xyz>
2016-11-08 14:17:30 +0100

    Add default contact value to /account/settings

--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -6,7 +6,7 @@
         <div class="panel-heading">
             <h4><i class="fa fa-1x fa-twitch"></i> Twitch account connection</h4>
         </div>
-        
+
         <div class="panel-body">
             @if (empty($twitch))
                 <a href="{{ route('auth.twitch.redirect') }}" class="btn btn-twitch">
@@ -15,7 +15,7 @@
             @else
                 Connected to <strong>{{ $twitch->nickname }}</strong>
                 {{-- <a class="pull-right" href="{{ route('auth.twitch.disconnect') }}"><i class="fa fa-1x fa-sign-out"></i> Disconnect?</a> --}}
-                <span class="text-primary pull-right">If you wish to disconnect your Twitch account, please do the following: {!! Markdown::convertToHtml(env('CONTACT')) !!}</span>
+                <span class="text-primary pull-right">If you wish to disconnect your Twitch account, please do the following: {!! Markdown::convertToHtml(env('CONTACT'), "Contact the /r/Twitch mods via [modmail](https://www.reddit.com/message/compose?to=%2Fr%2FTwitch)") !!}</span>
             @endif
         </div>
     </div>
