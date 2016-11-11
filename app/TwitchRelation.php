e448d7907bbf9dc5668fd2eba07971ad629e93cd
Alex Thomassen <alex@thomassen.xyz>
2016-11-11 17:24:59 +0100

    Update TwitchRelation to point to the right field

--- app/TwitchRelation.php
+++ app/TwitchRelation.php
@@ -43,6 +43,6 @@ class TwitchRelation extends Model
      */
     public function user()
     {
-        return $this->belongsTo('App\User', 'id', 'user_id');
+        return $this->belongsTo('App\User', 'user_id', 'id');
     }
 }
