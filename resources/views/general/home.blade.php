15746fa32a17c7b1f4ac1a03009e51fe102e6a20
Alex Thomassen <alex@thomassen.xyz>
2016-12-11 17:26:04 +0100

    Remove "work in progress" text

--- resources/views/general/home.blade.php
+++ resources/views/general/home.blade.php
@@ -15,16 +15,4 @@
             If you wish to submit a request, or you want to see the status of your current requests, please <a href="{{ route('auth.reddit.redirect') }}">login with your <i class="fa fa-1x fa-reddit"></i> Reddit account</a>.
         </p>
     @endif
-
-    <div class="alert alert-danger">
-        <strong>PLEASE READ!</strong>
-        <br>
-        While you are on this demo page, everything about this is "work in progress". Especially the design.
-        <br>
-        Most of the current design is purely there as a "proof-of-concept". Once the functionality is pretty much finished, there will be more focus on cleaning up the layout.
-        <br>
-        For now, just mess around and see if anything breaks.
-        <br><br>
-        As a sidenote: Both requests and comments support <strong>Markdown</strong>.
-    </div>
 @endsection
