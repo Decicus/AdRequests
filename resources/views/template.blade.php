2a030ea9168b326c683acdff5534a84810a1ed68
Alex Thomassen <alex@thomassen.xyz>
2017-01-03 08:47:18 +0100

    Load JS files locally instead of from CDN (for certain countries where CDNs are blocked)

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -157,8 +157,8 @@
             @yield('main')
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="{{ url('assets/js/jquery-3.1.1.min.js') }}" charset="utf-8"></script>
+        <script src="{{ url('assets/js/bootstrap.min.js') }}" charset="utf-8"></script>
         @yield('scripts')
     </body>
 </html>
