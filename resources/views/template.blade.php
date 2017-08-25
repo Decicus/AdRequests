27ad21d5e2663172a3c4456dab4c34dcd5cc01f7
Alex Thomassen <alex@thomassen.xyz>
2017-08-25 10:45:32 +0200

    Add support for Google Analytics

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -165,6 +165,17 @@
 
         <script src="{{ url('assets/js/jquery-3.1.1.min.js') }}" charset="utf-8"></script>
         <script src="{{ url('assets/js/bootstrap.min.js') }}" charset="utf-8"></script>
+        @if (!empty(env('GOOGLE_ANALYTICS_ID')))
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', '{{ env('GOOGLE_ANALYTICS_ID') }}', 'auto');
+                ga('send', 'pageview');
+            </script>
+        @endif
         @yield('scripts')
     </body>
 </html>
